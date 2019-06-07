67851c1da4a42beef02034266f3a0167e02171c1
soheila <soheilalotfi@ymail.com>
2019-06-07 12:25:41 -0400

    created variables and a random letter in game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+ // create variables
+ var wins=0;
+ var losses=0;
+ var guessesLeft=9;
+ var yourGuessessofar=[];
+ var computerChoices=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
+ //creat a random letter 
+ var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+ 
+ 

c43b94d1ab7af30a3b9d7723b805e37897430962
soheila <soheilalotfi@ymail.com>
2019-06-07 12:41:15 -0400

    created a function to get user input- used if statement to compare userguess with computerguess-if they are equel user wins and if they are not the user loses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,4 +8,35 @@
  //creat a random letter 
  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
  
+ //  get user input 
+ document.onkeyup = function(event){
+
+ var userGuess=event.key;
+ console.log(computerGuess);
+    
+ // compare userGuess with ComputerGuess, if they are not equal, the user loses 
+    if ( userGuess!== computerGuess){
  
+       guessesLeft--;
+       yourGuessessofar.push(userGuess);
+    }
+// compare userGuess with ComputerGuess, if they are equal, the user wins
+   if (userGuess===computerGuess){
+
+       wins++;
+       restartGame();
+     }
+// if the user couldnot guess the correct letter after 9 times, reset the game and increase the losses
+   if (guessesLeft === 0){
+
+       losses++;
+       restartGame();
+      }
+ 
+      function restartGame(){
+        yourGuessessofar=[];
+        guessesLeft=9;
+        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+      }
+
+ }
